057937bba28b2f079392b2bcd8081eceda94f0bc
Onyechere-benedict <onyecherebenedict@gmail.com>
2024-11-25 12:13:02 +0100

    Benedict's Commmit

---
+++ js/nav.js
@@ -0,0 +1,26 @@
+
+$(function () {
+    window.onscroll = function () {
+
+        const mediaQuery = window.matchMedia('(max-width: 1199px)');
+
+        if (!mediaQuery.matches) {
+
+            if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
+                $("nav").css("top", "-80px");
+            }
+            else {
+                $("nav").css("top", "45px");
+            }
+
+            if (document.body.scrollTop >= 400 || document.documentElement.scrollTop >= 400) {
+                $("nav .logo img").attr("src", "/images/logo2.jpeg");
+                $("nav").addClass("sticky");
+            }
+            else {
+                $("nav .logo img").attr("src", "/images/logo-removebg-preview.png");
+                $("nav").removeClass("sticky");
+            }
+        }
+    };
+});
